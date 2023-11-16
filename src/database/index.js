a9acd5ccab7a88de307abd84e09c225c12df870b
Sangani Akshay <114087974+Akki-patel@users.noreply.github.com>
2023-11-16 14:56:12 +0530

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -4,8 +4,7 @@ const configOptions={
     UseUnifiedTopology : true,
 };
 const connectToDB = async () => {
-  const connectionurl =
-    "mongodb+srv://sanganiakshay101:12345@cluster0.ccw6waa.mongodb.net/?retryWrites=true&w=majority";
+  const connectionurl ="add your cluster url here...";
      mongoose.connect(connectionurl,configOptions).then(()=>console.log('data base connected successfully')).catch((err)=>console.log('getting erroe from db connection ${err.message}'))
 };
 export default connectToDB;
