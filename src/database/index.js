41a0c9da67da13c92f33c0dab8719fc3439adb4d
Sangani Akshay <114087974+Akki-patel@users.noreply.github.com>
2024-01-21 19:23:30 +0530

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -5,7 +5,7 @@ const configOptions={
 };
 const connectToDB = async () => {
   const connectionurl =
-    "mongodb+srv://sanganiakshay101:12345@cluster0.n1ydodg.mongodb.net/";
+    "add your url";
      mongoose.connect(connectionurl,configOptions).then(()=>console.log('data base connected successfully')).catch((err)=>console.log('getting erroe from db connection ${err.message}'))
 };
 export default connectToDB;
